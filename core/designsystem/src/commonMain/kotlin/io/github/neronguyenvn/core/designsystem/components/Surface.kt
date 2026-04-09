2664fcba6b53c815d9544eb85677f05487676a7a
neronguyenvn <neronguyenvn@gmail.com>
2026-04-10 00:08:28 +0700

    feat: implement `NeroSurface` component in `:core:designsystem`
    - Create `NeroSurface` as a reusable layout component with a background, an optional header, and a scrollable content area.
    - Configure `NeroSurface` with a `RoundedCornerShape` for the content section to distinguish it from the background.
    - Add a `@PreviewLightDark` preview for `NeroSurface` showcasing its usage with a logo header and text content.

---
+++ core/designsystem/src/commonMain/kotlin/io/github/neronguyenvn/core/designsystem/components/Surface.kt
@@ -0,0 +1,89 @@
+package io.github.neronguyenvn.core.designsystem.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import io.github.neronguyenvn.core.designsystem.theme.NeroTheme
+import kmpnerochatapp.core.designsystem.generated.resources.Res
+import kmpnerochatapp.core.designsystem.generated.resources.logo_chirp
+import org.jetbrains.compose.resources.vectorResource
+
+@Composable
+fun NeroSurface(
+    modifier: Modifier = Modifier,
+    header: @Composable (ColumnScope.() -> Unit)? = null,
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            if (header != null) {
+                header()
+            }
+
+            Surface(
+                color = MaterialTheme.colorScheme.surface,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(
+                    topStart = 16.dp,
+                    topEnd = 16.dp
+                )
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 24.dp)
+                        .padding(horizontal = 16.dp)
+                        .verticalScroll(rememberScrollState())
+                ) {
+                    content()
+                }
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+fun NeroSurfacePreview() {
+    NeroTheme {
+        NeroSurface(
+            header = {
+                Icon(
+                    imageVector = vectorResource(Res.drawable.logo_chirp),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(vertical = 32.dp)
+                )
+            },
+            content = {
+                Text(
+                    text = "Welcome to Nero Chat!",
+                    style = MaterialTheme.typography.titleLarge,
+                )
+            }
+        )
+    }
+}
