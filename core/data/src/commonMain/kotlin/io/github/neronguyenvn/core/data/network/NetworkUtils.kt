c1c73ba855f3c52cb13e53d5bddab88796cf5eb9
neronguyenvn <neronguyenvn@gmail.com>
2026-04-18 17:55:56 +0700

    fix: use HTTP PUT method in network utility
    - Update the network request helper in `NetworkUtils.kt` to use Ktor's `put` method instead of `post`.
    - Add the necessary `io.ktor.client.request.put` import.

--- core/data/src/commonMain/kotlin/io/github/neronguyenvn/core/data/network/NetworkUtils.kt
+++ core/data/src/commonMain/kotlin/io/github/neronguyenvn/core/data/network/NetworkUtils.kt
@@ -11,6 +11,7 @@ import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
 import io.ktor.client.request.post
+import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
@@ -47,7 +48,7 @@ suspend inline fun <reified Request, reified Response : Any> HttpClient.put(
     queryParams: Map<String, Any> = mapOf(),
     noinline block: HttpRequestBuilder.() -> Unit = {}
 ): Either<DataError.Network, Response> = safeCall {
-    post {
+    put {
         setBody(body)
         prepareRequest(route, queryParams, block)
     }
